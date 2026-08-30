d5f5bece95aebb48305c48396e0482b7d190010d
Edilson <edilsonjunior101@gmail.com>
2026-08-30 18:17:24 -0300

    classes cliente e colaborador adicionadas

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -1,4 +1,58 @@
 package modelo;
 
 public class Cliente {
+    private String idCliente;
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String email;
+
+    public Cliente(String idCliente, String nome, String endereco, String telefone, String email){
+        this.idCliente = idCliente;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public String getIdCliente(){
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente){
+        this.idCliente = idCliente;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getEndereco(){
+        return endereco;
+    }
+
+    public void setEndereco(String endereco){
+        this.endereco = endereco;
+    }
+
+    public String getTelefone(){
+        return telefone;
+    }
+
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+
+    public String email(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
 }
--- src/modelo/Colaborador.java
+++ src/modelo/Colaborador.java
@@ -1,4 +1,78 @@
 package modelo;
 
 public class Colaborador {
+    private String idColaborador;
+    private String nome;
+    private String endereco;
+    private String email;
+    private String login;
+    private String senha;
+    private boolean admin;
+
+    public Colaborador(String idColaborador, String nome, String endereco, String email, String login, String senha, boolean admin){
+        this.idColaborador = idColaborador;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.email = email;
+        this.login = login;
+        this.senha = senha;
+        this.admin = admin;
+    }
+
+    public String getIdColaborador(){
+        return idColaborador;
+    }
+
+    public void setIdColaborador(String idColaborador){
+        this.idColaborador = idColaborador;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getEndereco(){
+        return endereco;
+    }
+
+    public void setEndereco(String endereco){
+        this.endereco = endereco;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getLogin(){
+        return login;
+    }
+
+    public void setLogin(String login){
+        this.login = login;
+    }
+
+    public String getSenha(){
+        return senha;
+    }
+
+    public void getSenha(String senha){
+        this.senha = senha;
+    }
+
+    public boolean getAdmin(){
+        return admin;
+    }
+
+    public void setAdmin(boolean admin){
+        this.admin = false;
+    }
+
 }
